505aec788e9e055e679bfcc2c02327131bef7f73
Vinicius Fernandes <viniviniciusfernandes15@gmail.com>
2025-06-09 21:38:08 -0300

    aprendendo sobre a estrutura e anotações do Spring

--- produtosapi/src/main/java/io/github/ovini05/produtosapi/ProdutosapiApplication.java
+++ produtosapi/src/main/java/io/github/ovini05/produtosapi/ProdutosapiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProdutosapiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProdutosapiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ProdutosapiApplication.class, args);
+    }
 
 }
