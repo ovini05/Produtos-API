fee82e29fd30d4ee742f9e24cb693b17e6112021
Vinicius Fernandes <viniviniciusfernandes15@gmail.com>
2025-06-10 00:23:59 -0300

    Criação do metodo de BuscaPorParametros

--- produtosapi/src/main/java/io/github/ovini05/produtosapi/controller/ProdutoController.java
+++ produtosapi/src/main/java/io/github/ovini05/produtosapi/controller/ProdutoController.java
@@ -4,6 +4,7 @@ import io.github.ovini05.produtosapi.model.Produto;
 import io.github.ovini05.produtosapi.repository.ProdutoRepository;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -41,6 +42,11 @@ public class ProdutoController {
     public void atualizar(@PathVariable("id") String id, @RequestBody Produto produto){
         produto.setId(id);
         produtoRepository.save(produto);
+    }
+
+    @GetMapping
+    public List<Produto> buscar(@RequestParam("nome") String nome){
+        return produtoRepository.findByNome(nome);
     }
 
 }
--- produtosapi/src/main/java/io/github/ovini05/produtosapi/repository/ProdutoRepository.java
+++ produtosapi/src/main/java/io/github/ovini05/produtosapi/repository/ProdutoRepository.java
@@ -3,5 +3,9 @@ package io.github.ovini05.produtosapi.repository;
 import io.github.ovini05.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto,String> {
+
+    List<Produto> findByNome(String nome);
 }
